e852d90d300664dae9d1bc15042e63fbd6fe8b12
Mitch Merrell <mitchmerrell@Mitchs-MacBook-Pro.local>
2019-09-02 09:46:28 -0600

    Day 1 stretch + diamond

--- AnchorPoint6StretchProblem.playground/Pages/6.1 - Game of Threes.xcplaygroundpage/Contents.swift
+++ AnchorPoint6StretchProblem.playground/Pages/6.1 - Game of Threes.xcplaygroundpage/Contents.swift
@@ -10,10 +10,53 @@
  Example: Input: 100 Output: [100, 33, 11, 4, 1]
  
  ### Black Diamond 💎💎💎
- 1. Instead of returning an array, return a dictionary where the keys are the values from the array, and the keys are 0, -1, or 1 depending on how the number was manipulated to make it divisible by 3
+ 1. Instead of returning an array, return a dictionary where the keys are the values from the array, and the values are 0, -1, or 1 depending on how the number was manipulated to make it divisible by 3
  2. Sadly, the key value pairs won't be ordered :( Example: Input: 100 Output: [100 : -1 , 33 : 0, 11 : 1, 4 : -1 , 1 : 0]
 */
 
 import UIKit
+
+func threes(num: Int) -> [Int] {
+    var nextNum = num
+    var numResults: [Int] = []
+    
+    while nextNum > 1 {
+        if (nextNum + 1) % 3 == 0 {
+            numResults.append(nextNum)
+            nextNum += 1
+        } else if (nextNum - 1) % 3 == 0 {
+            numResults.append(nextNum)
+            nextNum -= 1
+        } else {
+            numResults.append(nextNum)
+            nextNum /= 3
+        }
+    }
+    
+    return numResults
+}
+
+func threesDict(num: Int) -> [Int : Int] {
+    var nextNum = num
+    var numResults: [Int : Int] = [:]
+    
+    while nextNum > 1 {
+        if (nextNum + 1) % 3 == 0 {
+            numResults[nextNum] = 1
+            nextNum += 1
+        } else if (nextNum - 1) % 3 == 0 {
+            numResults[nextNum] = -1
+            nextNum -= 1
+        } else {
+            numResults[nextNum] = 0
+            nextNum /= 3
+        }
+    }
+    
+    return numResults
+}
+
+print(threes(num: 88))
+print(threesDict(num: 88))
 
 //: [Next](@next)
